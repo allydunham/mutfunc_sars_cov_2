05aec2b6fcc49a906d08aaf9026c38492123484b
allydunham <ally@ebi.ac.uk>
2020-06-19 15:26:43 +0100

    Updated to pull in data from each interface subdirectory

--- bin/complex_tsv.py
+++ bin/complex_tsv.py
@@ -8,32 +8,35 @@ import pandas as pd
 from ruamel.yaml import YAML
 from pathlib import Path
 
-def import_complex_dir(path):
+def import_complex_dir(path, chains):
     """
     Import tables from an AnalyseComplex output directory
     """
     path = path.rstrip('/')
-    yaml_parser = YAML(typ='safe')
-    proteins = yaml_parser.load(Path(f'{path}/model.yaml'))
     interactions = pd.read_csv(f'{path}/interactions.tsv', sep='\t')
     interactions = interactions.rename({'interface_residues': 'number_of_interface_residues'},
                                        axis='columns')
     interface = pd.read_csv(f'{path}/interface_residues.tsv', sep='\t')
     comb = pd.merge(interactions, interface, how='outer', on=['chain', 'position', 'wt', 'mut'])
     comb = comb.rename({'chain': 'mut_chain'}, axis='columns')
-    chain1 = comb.chain1[0]
-    chain2 = comb.chain2[0]
-    comb['uniprot1'] = proteins[chain1]['uniprot']
-    comb['protein1'] = proteins[chain1]['name']
-    comb['uniprot2'] = proteins[chain2]['uniprot']
-    comb['protein2'] = proteins[chain2]['name']
+    comb['uniprot1'] = [chains[chain]['uniprot'] for chain in comb.chain1]
+    comb['protein1'] = [chains[chain]['protein'] for chain in comb.chain1]
+    comb['uniprot2'] = [chains[chain]['uniprot'] for chain in comb.chain2]
+    comb['protein2'] = [chains[chain]['protein'] for chain in comb.chain2]
     cols = ['uniprot1', 'protein1', 'chain1', 'uniprot2', 'protein2', 'chain2']
     comb = comb[cols + [c for c in comb.columns if not c in cols]]
     return comb
 
 def main(args):
     """Main"""
-    complex_dfs = [import_complex_dir(d) for d in args.dirs]
+    complex_dfs = []
+    yaml_loader = YAML(typ='safe')
+    for yaml in args.yaml:
+        path = Path(yaml)
+        yaml = yaml_loader.load(path)
+        for interface in yaml['interfaces']:
+            complex_dfs.append(import_complex_dir(f'{path.parent}/{interface}'), yaml['chains'])
+
     complexes = pd.concat(complex_dfs)
     sort_cols = ['uniprot1', 'protein1', 'uniprot2', 'protein2', 'mut_chain', 'position', 'mut']
     complexes = complexes.sort_values(axis='rows', by=sort_cols).reset_index(drop=True)
@@ -44,7 +47,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('dirs', metavar='D', nargs='+', help="Directories to pull output from")
+    parser.add_argument('yaml', metavar='Y', nargs='+',
+                        help="YAML config files indicating location of each interface output")
 
     return parser.parse_args()
 
