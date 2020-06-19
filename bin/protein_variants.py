8c9f03dbc29a1605425ee09f9003f54a9d43a86e
allydunham <ally@ebi.ac.uk>
2020-06-19 15:26:08 +0100

    Added options to sort and make variants unique

--- bin/protein_variants.py
+++ bin/protein_variants.py
@@ -8,7 +8,12 @@ AMINO_ACIDS = 'ACDEFGHIKLMNPQRSTVWY'
 
 def main(args):
     """Main"""
-    for position in args.positions:
+    wt_aas = set(args.positions) if args.unique else args.positions
+
+    if args.sort is not None:
+        wt_aas = sorted(list(wt_aas), key=lambda x: int(x[args.sort:]))
+
+    for position in wt_aas:
         for mut in AMINO_ACIDS:
             if args.exclude is not None and position[args.exclude] == mut:
                 continue
@@ -20,10 +25,18 @@ def parse_args():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('positions', metavar='P', nargs='+', help="Protein positions to mutate")
+
     parser.add_argument('--exclude', '-e', default=None, type=int,
                         help="Do not mutate to the amino acid at this position in the string")
-    parser.add_argument('--suffix', '-s', default='\n',
+
+    parser.add_argument('--suffix', '-s', default='\n', type=str,
                         help="Append each mutation with this string")
+
+    parser.add_argument('--unique', '-u', action='store_true',
+                        help="Only generate unique variants")
+
+    parser.add_argument('--sort', '-o', default=None, type=int,
+                        help="Index of start of position in string, to sort on")
 
     return parser.parse_args()
 
