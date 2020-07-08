22a0da3dc21d9c9ac57b09dd8a656290bfc92a65
allydunham <ally@ebi.ac.uk>
2020-07-08 17:32:43 +0100

    Added file for storing SARS-CoV2 related constants

---
+++ frontend/src/lib/sars.js
@@ -0,0 +1,18 @@
+// Constants for SARS-CoV2
+
+export const sarsUniprot = {
+    'P0DTD1': 'rep1ab', 'P0DTC1': 'rep1a', 'P0DTC2': 's',
+    'P0DTC3': 'orf3a', 'P0DTC4': 'e', 'P0DTC5': 'm',
+    'P0DTC6': 'orf6', 'P0DTC7': 'orf7a', 'P0DTD8': 'orf7b',
+    'P0DTC8': 'orf8', 'P0DTC9': 'nc', 'A0A663DJA2': 'orf10',
+    'P0DTD2': 'orf9b', 'P0DTD3': 'orf14'
+}
+
+export const sarsGenes = ['nsp1', 'nsp2', 'nsp3','nsp4', 'nsp5',
+                          'nsp6','nsp7', 'nsp8', 'nsp9', 'nsp10',
+                          'nsp12', 'nsp13', 'nsp14', 'nsp15',
+                          'nsp16', 'nsp11', 's', 'orf3a', 'e',
+                          'm', 'orf6','orf7a', 'orf7b', 'orf8',
+                          'nc', 'orf10', 'orf9b','orf14']
+
+// TODO - Map for common versions of gene names
