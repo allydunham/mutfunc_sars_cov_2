cc1ff88d58febd7b5b34a9286fc1414381a96a7e
allydunham <ally@ebi.ac.uk>
2020-08-20 13:07:16 +0100

    Reworked footer to use normal html/css and position properly, plus corrected overflow problems in main app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,33 @@ import DataController from './components/DataController';
 import TitleBar from './components/TitleBar';
 import Footer from './components/Footer';
 import { ThemeProvider } from "@material-ui/styles";
+import { makeStyles } from '@material-ui/core/styles';
 import theme from './theme';
 
+const styles = makeStyles({
+    main: {
+        display: 'flex',
+        minHeight: '90vh',
+        flexDirection: 'column',
+        flex: 1,
+        overflow: 'hidden'
+    }
+});
+
 function App() {
+    const classes = styles()
     const [page, setPage] = useState('search')
 
     return (
         <ThemeProvider theme={theme}>
-            <TitleBar setPage={setPage}/>
-            {page === 'analysis'? <Analysis/> : null}
-            {page === 'help'? <Help/> : null}
-            {page === 'about'? <About/> : null}
-            {page === 'download'? <Download/> : null}
-            <DataController hidden={page !== 'search'}/>
+            <div className={classes.main}>
+                <TitleBar setPage={setPage}/>
+                {page === 'analysis'? <Analysis/> : null}
+                {page === 'help'? <Help/> : null}
+                {page === 'about'? <About/> : null}
+                {page === 'download'? <Download/> : null}
+                <DataController hidden={page !== 'search'}/>
+            </div>
             <Footer/>
         </ThemeProvider>
   );
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from "react";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const styles = makeStyles({
     separater: {
-        flexGrow: 1
+        flexGrow: 1,
+        alignSelf: 'flex-end'
     },
     footer: {
+        display: 'flex',
+        flexDirection: 'row',
         top: 'auto',
         bottom: 0,
+        paddingLeft: 10,
+        paddingRight: 10,
         background: 'transparent',
         boxShadow: 'none'
     }
@@ -18,16 +22,18 @@ const styles = makeStyles({
 const Footer = () => {
     const classes = styles()
     return(
-        <AppBar position='fixed' color='transparent' className={classes.footer}>
-            <Toolbar>
-                <img
-                  src={process.env.PUBLIC_URL + 'ebi_logo.svg'}
-                  alt='ebi-logo'
-                  width='10%'
-                />
-                <div className={classes.separater}/>
-            </Toolbar>
-        </AppBar>
+        <footer className={classes.footer}>
+            <img
+              src={process.env.PUBLIC_URL + 'ebi_logo.svg'}
+              alt='ebi-logo'
+              width='10%'
+            />
+            <div className={classes.separater}>
+                <Typography align='right'>
+                    v1.0 - Data Updated: 19/08/2020
+                </Typography>
+            </div>
+        </footer>
     )
 }
 
