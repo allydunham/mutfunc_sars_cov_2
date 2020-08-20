c6fd1af68a51fb07014da226f34b8f78ebe5400b
allydunham <ally@ebi.ac.uk>
2020-08-20 19:04:23 +0100

    Added proper spacing

--- frontend/src/components/Help.js
+++ frontend/src/components/Help.js
@@ -12,6 +12,15 @@ const styles = makeStyles({
         padding: '10px',
         width: '70%'
     },
+    heading: {
+        paddingTop: '50px'
+    },
+    subheading: {
+        paddingTop: '25px'
+    },
+    content: {
+        paddingTop: '25px',
+    },
     badgeList: {
         marginTop: 5,
         listStyle: 'none'
@@ -28,7 +37,7 @@ const Help = () => {
     const classes = styles()
     return(
         <div className={classes.root}>
-            <Typography>
+            <Typography className={classes.content}>
                 Mutfunc: SARS-CoV-2 provides a subset of <Link href='https://www.mutfunc.com'>Mutfunc</Link> variant effect predictions for SARS-CoV-2, the novel coronavirus responsible for COVID-19.
                 Mutfunc is a database of precomputed variant effect predictions from various tools for all <i>S. cerevisiae</i>, <i>H. sapians</i> and <i>E. coli</i> variants.
                 The main Mutfunc website has predictions conservation, structural consequences, protein interfaces, post-transational modifications, transcription factor binding sites and linear motifs.
@@ -36,10 +45,10 @@ const Help = () => {
 
                 The website provides an interface for searching variants online, downloading complete dataset and gives an overview analysis of the data.
             </Typography>
-            <Typography variant='h5'>
+            <Typography variant='h5' className={classes.heading}>
                 Searching Variants
             </Typography>
-            <Typography>
+            <Typography className={classes.content}>
                 The search interface allows you to search SARS-CoV-2 variants with a number of terms:
 
                 <ul>
@@ -57,10 +66,10 @@ const Help = () => {
                     <li>Gene names and Uniprot IDs are both case insensitive.</li>
                 </ul>
             </Typography>
-            <Typography variant='h5'>
+            <Typography variant='h5' className={classes.heading}>
                 Results Table
             </Typography>
-            <Typography>
+            <Typography className={classes.content}>
                 The mutations returned by your search are displayed in a table, by default filtered to only display variants with a significant predicted effect.
                 All identified variants can be displayed using the checkbox in the table controls.
                 Variants are predicted to be significant if they fulfil any of the following conditions:
@@ -91,16 +100,17 @@ const Help = () => {
                 The predictions column shows which factors are predicted to be significant.
                 Clicking on a variants row opens a panel with additional details on predictions.
             </Typography>
-            <Typography variant='h5'>
+            <Typography variant='h5' className={classes.heading}>
                 Predictions
             </Typography>
-            <Typography>
+            <Typography className={classes.content}>
                 Three categories of predictions and measurement are shown in hte details panel of each variant: conservation, structure and interfaces.
             </Typography>
-            <Typography variant='subtitle1'>
+            <Typography variant='h6' className={classes.subheading}>
                 Conservation
             </Typography>
-            <Typography>
+            <Typography className={classes.content}>
+                The conservation data consists of:
                 <ul>
                     <li>The observed frequency of the variant, calculated from the <Link href='https://www.gisaid.org/' target="_blank" rel="noopener noreferrer">GISAID</Link> datatset</li>
                     <li>Whether the position has an post-translational modifications (currently only phosphorylations from <Link href="https://europepmc.org/article/med/32645325" target="_blank" rel="noopener noreferrer"> Bouhaddou et al. (2020) </Link> are included)</li>
@@ -110,19 +120,19 @@ const Help = () => {
 
                 SIFT4G scores are often generated from reasonably closely related sequences and need to be interpretted with caution
             </Typography>
-            <Typography variant='subtitle1'>
+            <Typography variant='h6' className={classes.subheading}>
                 Structure
             </Typography>
-            <Typography>
+            <Typography className={classes.content}>
                 The structural consequences are calculated by <Link href="http://foldxsuite.crg.eu/" target="_blank" rel="noopener noreferrer">FoldX</Link> and summarised by the &Delta;&Delta;G statistic.
                 This measures the predicted stabilisation (&lt;0) or destabilisation (&gt;0) of the variant, with absolute values greater than one being considered significant.
                 The PDB model used is also listed as well as being viewable with the variant highlighted.
                 These models were identified using the <Link href="https://swissmodel.expasy.org/repository/species/2697049" target="_blank" rel="noopener noreferrer">SWISS-Model COVID Repository</Link> and contain a mixture of direct SARS-CoV-2 models and homology models.
             </Typography>
-            <Typography variant='subtitle1'>
+            <Typography variant='h6' className={classes.subheading}>
                 Interfaces
             </Typography>
-            <Typography>
+            <Typography className={classes.content}>
                 Protein interfaces, again gathered from the SWISS-Model COVID Repository, were also analysed using FoldX.
                 It identifies the amino acids involved in an inerface and assesses the properties of the interface.
                 The amino acids in the interfaces were computationally mutated to all other amino acids and the interface assessed again by FoldX to determine variants affect on stability.
@@ -130,7 +140,7 @@ const Help = () => {
                 Again the template and a viewable structure are available.
             </Typography>
 
-            <Typography>
+            <Typography className={classes.content}>
                 Additional breakdowns of all statistics, for example the factors contributing to FoldX's &Delta;&Delta;G statistic, are available in the full dataset.
             </Typography>
         </div>
