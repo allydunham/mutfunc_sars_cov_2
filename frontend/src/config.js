4fd77fba296fee8e4811a2299610163bebf713c0
allydunham <ally@ebi.ac.uk>
2020-11-03 17:59:08 +0000

    Added notes that frequencies are all-time/whole-world, not current and added list of current complexes (stored in config.js)

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,5 +2,9 @@
 export default {
     'version': '1.0.0',
     'update_date': '02/11/2020',
-    'vcf_version': '16/10/2020'
+    'vcf_version': '16/10/2020',
+    'complexes': ['S - ACE2', 'S homotrimer', 'Nucleocapsid homodimer',
+                  'Nsp1 - 40s Ribosome Subunit', 'Nsp7 - Nsp8', 'Nsp7 - Nsp8 - RdRp',
+                  'Nsp10 - ExoN', 'Nsp10 - Nsp16', 'Orf3a homodimer', 'Orf9b homodimer',
+                  'Orf9b - Tom70']
 }
