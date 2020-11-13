97d127513307ff678cdf35f36ba8639919d6de3c
allydunham <ally@ebi.ac.uk>
2020-11-13 20:27:13 +0000

    Added ROC curve panel for predicting DMS result

--- analysis/figure2.R
+++ analysis/figure2.R
@@ -10,36 +10,50 @@ spike <- read_csv('data/starr_ace2_spike.csv') %>%
   left_join(variants, by = c('uniprot', 'name', 'position', 'wt', 'mut'))
 
 ### Panel - Frequency
-p_freqs_per_int <- select(variants, name, position, wt, mut, int_name, freq, diff_interaction_energy) %>% 
-  drop_na(int_name) %>%
-  mutate(int_name = ifelse(str_detect(int_name, 'ribosomal'), '40s', int_name)) %>%
-  group_by(name, int_name) %>%
-  summarise(positions = n_distinct(position),
-            muts_per_pos = sum(freq, na.rm = TRUE) / positions,
-            mean_ddg = mean(diff_interaction_energy * freq, na.rm = TRUE),
-            .groups = 'drop') %>%
-  mutate(mean_ddg = ifelse(is.na(mean_ddg), 0, mean_ddg),
-         name = display_names[name],
+sig_vars <- select(variants, name, position, wt, mut, int_name, freq, diff_interaction_energy) %>% 
+  drop_na(int_name, freq) %>%
+  mutate(name = display_names[name],
+         int_name = display_names[int_name],
+         int = str_c(name, ' - ', int_name), 
+         sig = ifelse(diff_interaction_energy < 1, ifelse(diff_interaction_energy < -1, 'Stabilising', 'Neutral'), 'Destabilising'))
+
+p_freq <- ggplot(mapping = aes(x = freq, y = int)) +
+  geom_point(data = filter(sig_vars, sig == 'Neutral'), mapping = aes(colour = 'Neutral'), shape = 20, size = 0.4, position = position_jitter(height = 0.2)) +
+  geom_point(data = filter(sig_vars, sig == 'Stabilising'), mapping = aes(colour = 'Stabilising'), shape = 16, size = 0.8) +
+  geom_point(data = filter(sig_vars, sig == 'Destabilising'), mapping = aes(colour = 'Destabilising'), shape = 16, size = 0.8) +
+  scale_x_log10() +
+  scale_colour_manual(name = '', values = c(Neutral='gray30', Stabilising='#377eb8', Destabilising='#e41a1c')) +
+  labs(x = 'Variant Frequency') +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(linetype = 'dotted', colour = 'grey'),
+        legend.position = 'top',
+        legend.margin = margin(l = -5, b = -5, unit = 'mm'),
+        legend.spacing.x = unit(-1, 'mm'))
+
+### Panel - Stability
+p_stability <- select(variants, name, position, wt, mut, int_name, diff_interaction_energy) %>% 
+  mutate(name = display_names[name],
          int_name = display_names[int_name],
          int = str_c(name, ' - ', int_name)) %>%
-  ggplot(aes(x = muts_per_pos, y = mean_ddg)) +
-  geom_point(aes(size = positions), colour = '#984ea3') +
-  geom_text_repel(aes(label = int)) +
-  scale_x_log10() +
-  scale_y_log10() +
-  scale_size_area(name = 'Interface\nPositions') +
-  coord_cartesian(clip = 'off') +
-  labs(x = 'Variants per position per strain', y = expression('Frequency weighted mean interface'~Delta*Delta*G~'(kJ'%.%'mol'^-1*')'))
-
-### Panel - Frequency vs FoldX
-p_freq <- select(variants, position, wt, mut, freq, int_name, diff_interaction_energy) %>%
-  drop_na(int_name) %>%
-  mutate(freq_cat = classify_freq(freq)) %>%
-  ggplot() +
-  geom_boxplot(mapping = aes(x = freq_cat, y = diff_interaction_energy), fill = '#984ea3') +
-  geom_hline(yintercept = 1, linetype = 'dotted', colour = 'black') +
-  geom_hline(yintercept = -1, linetype = 'dotted', colour = 'black') +
-  labs(x = 'Variant Frequency (%)', y = expression('FoldX Interface'~Delta*Delta*G~'(kJ'%.%'mol'^-1*')'))
+  group_by(name) %>%
+  mutate(pos_prop = position / max(position),
+         width = 1 / max(position)) %>%
+  filter(!all(is.na(int))) %>%
+  group_by(int, position, pos_prop, width) %>%
+  summarise(mean_ddg = mean(clamp(diff_interaction_energy, -5, 5)), .groups='drop') %>%
+  drop_na() %>%
+  ggplot(aes(x = pos_prop, y = int, fill = mean_ddg, width = width)) +
+  geom_tile(height=0.5, show.legend = FALSE) + 
+  scale_fill_gradientn(colours = c('#5e4fa2', '#3288bd', '#66c2a5', '#abdda4', '#e6f598', '#ffffbf',
+                                   '#fee08b', '#fdae61', '#f46d43', '#d53e4f', '#9e0142'),
+                       values = scales::rescale(-5:5, to=0:1), na.value = 'white',
+                       limits = c(-5, 5), name="Mean interface &Delta;&Delta;G (kJ.mol<sup>-1</sup>)<br>Clamped to &plusmn;5") +
+  labs(x = 'Proportional Position') +
+  theme(legend.title = element_markdown(),
+        axis.title.y = element_blank(), axis.ticks.y = element_blank(),
+        panel.grid.major.y = element_line(linetype = 'solid', colour = 'grey'),
+        panel.grid.major.x = element_line(linetype = 'dotted', colour = 'grey'))
 
 ### Panel - Spike DMS vs. FoldX
 p_spike_dms <- select(spike, position, wt, mut, binding, int_name, diff_interaction_energy) %>%
@@ -48,7 +62,39 @@ p_spike_dms <- select(spike, position, wt, mut, binding, int_name, diff_interact
   ggplot(aes(x = sig, y = binding)) +
   geom_boxplot(fill = '#984ea3') +
   stat_compare_means(comparisons = list(c('Destabilising', 'Neutral'), c('Stabilising', 'Neutral')), method = 't.test', size = 2) +
-  labs(x = 'FoldX Interface Prediction', y = 'Spike DMS ACE2 Binding Fitness')
+  coord_cartesian(clip = 'off') +
+  labs(x = 'FoldX Interface Prediction', y = 'ACE2 Binding Fitness')
+
+### Panel - Spike DMS model
+dms_models <- select(spike, binding, ddg=total_energy, int_ddg = diff_interaction_energy, sift_score) %>%
+  mutate(binding_sig = binding < log10(0.5)) %>% # Binding rate half wt
+  pivot_longer(c(-binding, -binding_sig), names_to = 'tool', values_to = 'score') %>%
+  group_by(tool) %>%
+  group_modify(~calc_roc(., binding_sig, score, greater = .y$tool != 'sift_score'))
+
+p_roc <- ggplot(dms_models, aes(x = fpr, y = tpr, colour = tool)) +
+  geom_step(direction = 'hv') +
+  geom_abline(slope = 1, linetype = 'dashed', colour = 'black') +
+  labs(x = 'False Positive Rate', y = 'True Positive Rate') +
+  scale_colour_brewer(type = 'qual', palette = 'Dark2', name = '',
+                      labels = c(ddg=expression(Delta*Delta*G),
+                                 int_ddg=expression('Interface'~Delta*Delta*G),
+                                 sift_score='SIFT4G Score')) +
+  # Interfaces
+  annotate('segment', x = 0.5439189, y = 0.6075949, xend = 0.57, yend = 0.45) +
+  annotate('text', x = 0.57, y = 0.44, label = 'Delta*Delta*"G Threshold"%~~%0', hjust = 0.1, vjust = 1, parse = TRUE, size = 2) +
+  theme(legend.position = 'top',
+        legend.margin = margin(l = -5, b = -5, unit = 'mm'),
+        legend.spacing.x = unit(0, 'mm'))
+
+p_pr <- ggplot(dms_models, aes(x = tpr, y = precision, colour = tool)) +
+  geom_step(direction = 'hv') +
+  geom_hline(yintercept = 0.5, linetype = 'dashed', colour = 'black') +
+  labs(x = 'False Positive Rate', y = 'True Positive Rate') +
+  scale_colour_brewer(type = 'qual', palette = 'Dark2', name = '',
+                      labels = c(ddg=expression(Delta*Delta*G),
+                                 int_ddg=expression('Interface'~Delta*Delta*G),
+                                 sift_score='SIFT4G Score'))
 
 ### Panel - Orf3a in vitro DMS
 is_dms <- select(variants, name, position, wt, mut, log10_freq, int_name, diff_interaction_energy) %>%
@@ -104,11 +150,13 @@ p_is_dms <- ggplot() +
 size <- theme(text = element_text(size = 8))
 p1 <- p_freq + labs(tag = 'A') + size
 p2 <- p_spike_dms + labs(tag = 'B') + size
-p3 <- p_is_dms + labs(tag = 'C') + size
+p3 <- p_roc + labs(tag = 'C') + size
+p4 <- p_is_dms + labs(tag = 'D') + size
 
 figure <- multi_panel_figure(width = c(60, 60, 60), height = c(50, 65, 65), panel_label_type = 'none', row_spacing = 0, column_spacing = 0) %>%
   fill_panel(p1, row = 1, column = 1) %>%
-  fill_panel(p2, row = 3, column = 1) %>%
-  fill_panel(p3, row = 2:3, column = 2:3)
+  fill_panel(p2, row = 1, column = 2) %>%
+  fill_panel(p3, row = 1, column = 3) %>%
+  fill_panel(p4, row = 2:3, column = 2:3)
 ggsave('figures/figures/figure2.pdf', figure, width = figure_width(figure), height = figure_height(figure), units = 'mm')
 ggsave('figures/figures/figure2.png', figure, width = figure_width(figure), height = figure_height(figure), units = 'mm')
