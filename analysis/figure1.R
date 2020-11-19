25553c0f7d7c7ea61b7a70a06d179ffbfd55d88a
allydunham <ally@ebi.ac.uk>
2020-11-19 18:26:36 +0000

    Initial version of schematic

--- analysis/figure1.R
+++ analysis/figure1.R
@@ -2,6 +2,7 @@
 # Generate figure 1 (Summary of the data sources/pipeline, stats of results/coverage, benchmark against frequency)
 source('src/config.R')
 source('src/analysis.R')
+library(png)
 
 variants <- load_variants()
 spike <- read_csv('data/starr_ace2_spike.csv') %>% 
@@ -10,7 +11,70 @@ spike <- read_csv('data/starr_ace2_spike.csv') %>%
   left_join(variants, by = c('uniprot', 'name', 'position', 'wt', 'mut'))
 
 ### Panel 1 - Schematic
-p_schematic <- blank_plot(text = 'Schematic')
+ensembl_img <- readPNG('figures/figures/misc_parts/ensembl_logo.png')
+gisaid_img <- readPNG('figures/figures/misc_parts/gisaid_logo.png')
+pdbe_img <- readPNG('figures/figures/misc_parts/pdbe_logo.png')
+swissmodel_img <- readPNG('figures/figures/misc_parts/swissmodel.png')
+uniprot_img <- readPNG('figures/figures/misc_parts/uniprot.png')
+
+protein_img <- readPNG('figures/figures/misc_parts/protein.png')
+interface_img <- readPNG('figures/figures/misc_parts/interface.png')
+alignment_img <- readPNG('figures/figures/misc_parts/alignment.png')
+sequence_img <- readPNG('figures/figures/misc_parts/sequence.png')
+
+sift_img <- readPNG('figures/figures/misc_parts/sift_logo.png')
+foldx_img <- readPNG('figures/figures/misc_parts/foldx_logo.png')
+
+title_size = 3.5
+main_size = 2.5
+panel_colour <- '#faebff'
+p_schematic <- ggplot() +
+  coord_fixed(expand = FALSE, xlim = c(0, 3), ylim = c(0, 1), clip = 'off') +
+  scale_x_continuous(breaks = c(0.5, 1.5, 2.5), labels = c('Sources', 'Data', 'Predictions'), position = 'top') +
+  
+  # Boxes and arrows
+  annotate('TextBox', x = 0.5, y = 1, label = '', width = unit(0.28, 'native'), height = unit(1, 'native'),
+           hjust = 0.5, vjust = 1, fill = panel_colour, colour = NA) +
+  annotate('segment', x = 1, y = 0.5, xend = 1.01, yend = 0.5, arrow = arrow(length = unit(0.05, 'native'), type = 'closed')) +
+  annotate('TextBox', x = 1.5, y = 1, label = '', width = unit(0.3, 'native'), height = unit(1, 'native'),
+           hjust = 0.5, vjust = 1, fill = panel_colour, colour = NA) +
+  annotate('segment', x = 2.02, y = 0.5, xend = 2.03, yend = 0.5, arrow = arrow(length = unit(0.05, 'native'), type = 'closed')) +
+  annotate('TextBox', x = 2.5, y = 1, label = '', width = unit(0.28, 'native'), height = unit(1, 'native'),
+           hjust = 0.5, vjust = 1, fill = panel_colour, colour = NA) +
+  
+  # Sources
+  annotation_raster(uniprot_img, xmin = 0.33, xmax = 0.67, ymin = 0.81, ymax = 0.97) +
+  annotation_raster(swissmodel_img, xmin = 0.4, xmax = 0.6, ymin = 0.615, ymax = 0.765) +
+  annotation_raster(pdbe_img, xmin = 0.3, xmax = 0.7, ymin = 0.43, ymax = 0.55) +
+  annotate('text', x = 0.5, y = 0.3, label = 'Bouhaddou et al. (2020)', size = main_size) +
+  annotation_raster(ensembl_img, xmin = 0.2, xmax = 0.45, ymin = 0.05, ymax = 0.133) +
+  annotation_raster(gisaid_img, xmin = 0.575, xmax = 0.875, ymin = 0.05, ymax = 0.1625) +
+  
+  # Input Data
+  annotation_raster(sequence_img, xmin = 1.07, xmax = 1.47, ymin = 0.9, ymax = 0.93) +
+  annotate('text', label='Protein Sequences', x = 1.5, y = 0.915, vjust = 0.5, hjust = 0, size = main_size) +
+  annotation_raster(protein_img, xmin = 1.1, xmax = 1.4, ymin = 0.575, ymax = 0.875) +
+  annotate('text', label='Protein Structures', x = 1.5, y = 0.75, vjust = 0.5, hjust = 0, size = main_size) +
+  annotation_raster(interface_img, xmin = 1.1, xmax = 1.4, ymin = 0.35, ymax = 0.65) +
+  annotate('text', label='Complex Structures', x = 1.5, y = 0.5, vjust = 0.5, hjust = 0, size = main_size) +
+  annotate('segment', x = c(1.15, 1.25), xend = c(1.35, 1.25), y = c(0.3, 0.3), yend = c(0.3, 0.32)) + 
+  annotate('point', x = 1.25, y = 0.33, shape = 19, colour = 'red') + 
+  annotate('text', label='Phosphosites', x = 1.5, y = 0.3, vjust = 0.5, hjust = 0, size = main_size) +
+  annotation_raster(alignment_img, xmin = 1.07, xmax = 1.47, ymin = 0.05, ymax = 0.137) +
+  annotate('text', label='Sequence Alignments', x = 1.5, y = 0.1, vjust = 0.5, hjust = 0, size = main_size) +
+  
+  # Output
+  annotation_raster(sift_img, xmin = 2.25, xmax = 2.45, ymin = 0.75, ymax = 0.95) +
+  annotate('text', label='Conservation\nSIFT4G Score', x = 2.5, y = 0.865, vjust = 0.5, hjust = 0, size = main_size) +
+  annotation_raster(foldx_img, xmin = 2.2, xmax = 2.45, ymin = 0.568, ymax = 0.682) +
+  annotate('richtext', label='Protein stability &Delta;&Delta;G', x = 2.5, y = 0.675, vjust = 0.5, hjust = 0, fill = NA, label.color = NA, size = main_size) +
+  annotate('richtext', label='Interface stability &Delta;&Delta;G', x = 2.5, y = 0.575, vjust = 0.5, hjust = 0, fill = NA, label.color = NA, size = main_size) +
+  annotate('text', label='PTM Locations', x = 2.5, y = 0.3, vjust = 0, hjust = 0.5, size = main_size) +
+  annotate('text', label='Variant Frequency', x = 2.5, y = 0.1, vjust = 0, hjust = 0.5, size = main_size) +
+
+  # Theme
+  theme(axis.title = element_blank(), axis.text.y = element_blank(),
+        axis.ticks = element_blank(), panel.grid.major.y = element_blank())
 
 ### Panel 2 - Structural Coverage
 p_coverage <- select(variants, name, position, sift_score, total_energy) %>%
