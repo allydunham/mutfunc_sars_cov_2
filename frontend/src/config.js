9f5a70483264063244964977e4743d739c30cf8c
allydunham <ally@ebi.ac.uk>
2020-11-20 15:32:48 +0000

    Updated update date

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // Store site globals in a centralised location
 export default {
     'version': '1.0.0',
-    'update_date': '02/11/2020',
+    'update_date': '20/11/2020',
     'vcf_version': '16/10/2020',
     'complexes': ['S - ACE2', 'S homotrimer', 'Nucleocapsid homodimer',
                   'Nsp1 - 40s Ribosome Subunit', 'Nsp7 - Nsp8', 'Nsp7 - Nsp8 - RdRp',
