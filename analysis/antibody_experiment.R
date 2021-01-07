c5c8c5bc305b248b6a8ab5a889478ea9f1736715
allydunham <ally@ebi.ac.uk>
2021-01-07 18:00:16 +0000

    Added basic analysis of antibody escape dms

---
+++ analysis/antibody_experiment.R
@@ -0,0 +1,56 @@
+#!/usr/bin/env Rscript
+# Analyse results of Antibody binding DMS
+source('src/config.R')
+source('src/analysis.R')
+
+### Load Data ###
+antibody <- read_csv('data/greaney_spike_antibody.csv') %>%
+  tidyr::extract(condition, c('subject', 'day'), "subject ([A-Z]) \\(day ([0-9]*)\\)", convert=TRUE) %>%
+  rename(position=site, wt=wildtype, mut=mutation)
+
+antibody_averages <- group_by(antibody, position, wt, mut) %>%
+  summarise(mut_escape_mean = mean(mut_escape, na.rm = TRUE),
+            mut_escape_sd = sd(mut_escape, na.rm = TRUE),
+            mut_escape_median = median(mut_escape, na.rm = TRUE),
+            mut_escape_max = max(mut_escape, na.rm = TRUE),
+            mut_escape_min = min(mut_escape, na.rm = TRUE),
+            .groups = 'drop') %>%
+  mutate(name='s')
+
+variants <- load_variants() %>%
+  select(uniprot, name, position, wt, mut, freq, sift_score, total_energy, int_name, diff_interaction_energy) %>%
+  left_join(antibody_averages, by = c('name', 'position', 'wt', 'mut')) %>%
+  drop_na(mut_escape_mean) %>%
+  mutate(sift_sig = ifelse(sift_score < 0.05, 'Deleterious', 'Neutral'),
+         foldx_sig = ifelse(total_energy < 1, ifelse(total_energy < -1, 'Stabilising', 'Neutral'), 'Destabilising'),
+         int_sig = ifelse(diff_interaction_energy < 1, ifelse(diff_interaction_energy < -1, 'Stabilising', 'Neutral'), 'Destabilising'))
+
+### Analysis ###
+plots <- list()
+plots$sift_cat <- select(variants, sift_sig, mut_escape_mean, mut_escape_median, mut_escape_max) %>%
+  pivot_longer(-sift_sig, names_to = 'metric', values_to = 'mut_escape', names_prefix='mut_escape_') %>%
+  ggplot(aes(x = sift_sig, y = mut_escape)) +
+  facet_wrap(~metric) +
+  geom_boxplot(show.legend = FALSE) +
+  stat_compare_means(method = 't.test', comparisons = list(c('Deleterious', 'Neutral'))) +
+  labs(x = 'SIFT4G Prediction', y = 'Antibody Escape Proportion')
+
+plots$foldx_cat <- select(variants, foldx_sig, mut_escape_mean, mut_escape_median, mut_escape_max) %>%
+  pivot_longer(-foldx_sig, names_to = 'metric', values_to = 'mut_escape', names_prefix='mut_escape_') %>%
+  ggplot(aes(x = foldx_sig, y = mut_escape)) +
+  facet_wrap(~metric) +
+  geom_boxplot(show.legend = FALSE) +
+  stat_compare_means(method = 't.test', comparisons = list(c('Destabilising', 'Neutral'), c('Stabilising', 'Neutral'), c('Destabilising', 'Stabilising'))) +
+  labs(x = 'FoldX Prediction', y = 'Antibody Escape Proportion')
+
+plots$int_cat <- select(variants, int_name, int_sig, mut_escape_mean, mut_escape_median, mut_escape_max) %>%
+  drop_na(int_sig) %>%
+  pivot_longer(mut_escape_mean:mut_escape_max, names_to = 'metric', values_to = 'mut_escape', names_prefix='mut_escape_') %>%
+  ggplot(aes(x = int_sig, y = mut_escape)) +
+  facet_grid(rows = vars(int_name), cols = vars(metric)) +
+  geom_boxplot(show.legend = FALSE) +
+  stat_compare_means(method = 't.test', comparisons = list(c('Destabilising', 'Neutral'), c('Stabilising', 'Neutral'), c('Destabilising', 'Stabilising'))) +
+  labs(x = 'FoldX Interface Prediction', y = 'Antibody Escape Proportion')
+
+### Save plots ###
+save_plotlist(plots, 'figures/antibody_escape', verbose = 2, overwrite = 'all')
