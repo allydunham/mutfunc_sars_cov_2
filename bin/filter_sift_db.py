e3ae40efa089f9c9e63bc5f153a746ce55104ff7
allydunham <ally@ebi.ac.uk>
2021-01-25 14:27:02 +0000

    Added custom MMseqs2 based SIFT4G dir

--- bin/filter_sift_db.py
+++ bin/filter_sift_db.py
@@ -25,10 +25,6 @@ def filter_fastas(path):
             isolate != 'HCoV-EMC/2012'):
             continue
 
-        if (organism == 'Severe acute respiratory syndrome-related coronavirus' and
-            isolate is None):
-            continue
-
         yield seq
 
 def main(args):
