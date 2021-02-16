16cfe686e1b8f63b4aeec711d7f6dd84cf7c3b5f
allydunham <ally@ebi.ac.uk>
2021-02-16 18:12:42 +0000

    Added inverted figure

--- analysis/nsp2.R
+++ analysis/nsp2.R
@@ -25,7 +25,15 @@ plots$sift_dist_viol <- ggplot(variants, aes(x = freq_cat, y = sift_score)) +
   geom_hline(yintercept = 0.05, linetype = 'dotted', colour = 'black') +
   geom_point(data = filter(variants, freq_cat %in% c('0.1-1', '1-10', '> 10'))) +
   geom_text_repel(data = filter(variants, freq_cat %in% c('1-10', '> 10') | (freq_cat == '0.1-1' & sift_score < 0.05)),
-                  mapping = aes(label = str_c(wt, position, mut)), nudge_x = 0.3, force = 4) +
+                  mapping = aes(label = str_c(wt, position, mut)), nudge_x = c(0.4, -0.4), force = 4) +
   labs(x = 'Variant Frequency (%)', y = 'SIFT4G Score')
+
+plots$sift_log10_dist_viol <- ggplot(variants, aes(x = freq_cat, y = -log10_sift)) + 
+  geom_violin(fill = '#377eb8', size = 0.5) +
+  geom_hline(yintercept = -log10(0.05), linetype = 'dotted', colour = 'black') +
+  geom_point(data = filter(variants, freq_cat %in% c('0.1-1', '1-10', '> 10'))) +
+  geom_text_repel(data = filter(variants, freq_cat %in% c('1-10', '> 10') | (freq_cat == '0.1-1' & sift_score < 0.05)) %>% arrange(desc(freq_cat), log10_sift),
+                  mapping = aes(label = str_c(wt, position, mut)), nudge_x = c(0.4, -0.4), nudge_y = c(0, 0.1, 0, -0.1), force = 4) +
+  labs(x = 'Variant Frequency', y = expression(-log[10]~'SIFT4G Score'))
 
 save_plotlist(plots, 'figures/nsp2', overwrite = 'all')
