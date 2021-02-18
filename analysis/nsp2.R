3b48ad271a3ee353be0d56d9a90589bb841e0cd0
allydunham <ally@ebi.ac.uk>
2021-02-18 18:48:02 +0000

    Added FoldX plot

--- analysis/nsp2.R
+++ analysis/nsp2.R
@@ -3,8 +3,14 @@
 source('src/config.R')
 source('src/analysis.R')
 
+foldx <- read_tsv('data/nsp2_foldx/average.fxout') %>%
+  select(-chain)
+
 variants <- load_variants() %>% filter(name == 'nsp2') %>%
-  mutate(freq_cat = classify_freq(freq))
+  select(uniprot, name, position, wt, mut, freq, log10_freq, ptm, sift_score, log10_sift, sift_median) %>%
+  left_join(select(foldx, position, wt, mut, total_energy), by = c('position', 'wt', 'mut')) %>%
+  mutate(freq_cat = classify_freq(freq),
+         sift_sig = ifelse(sift_score < 0.05, 'Deleterious', 'Neutral'))
 
 plots <- list()
 plots$sift_dist <- select(variants, sift_score, freq) %>%
@@ -35,5 +41,15 @@ plots$sift_log10_dist_viol <- ggplot(variants, aes(x = freq_cat, y = -log10_sift
   geom_text_repel(data = filter(variants, freq_cat %in% c('1-10', '> 10') | (freq_cat == '0.1-1' & sift_score < 0.05)) %>% arrange(desc(freq_cat), log10_sift),
                   mapping = aes(label = str_c(wt, position, mut)), nudge_x = c(0.4, -0.4), nudge_y = c(0, 0.1, 0, -0.1), force = 4) +
   labs(x = 'Variant Frequency', y = expression(-log[10]~'SIFT4G Score'))
+
+plots$foldx <- ggplot(variants, aes(x = freq * 100, y = clamp(total_energy, -10, 10))) +
+  geom_point(aes(colour = sift_sig)) +
+  geom_text_repel(data = filter(variants, freq > 0.003 | (freq > 0.001 & abs(total_energy) > 1)),
+                  mapping = aes(label = str_c(wt, position, mut)), nudge_x = 0.3, size = 3, segment.size = 0.25) +
+  geom_hline(yintercept = c(-1, 1), linetype = 'dotted') +
+  scale_x_log10(labels = function(x){str_c(x, '%')}) +
+  scale_colour_manual(name = 'SIFT Prediction', values = c(Neutral='black', Deleterious='red')) +
+  labs(x = 'Frequency', y = expression(Delta*Delta*G~'(kJ'%.%'mol'^-1*', clamped to'%+-%'10)'),
+       subtitle = 'Nsp2 Structural Destabilisation')
 
 save_plotlist(plots, 'figures/nsp2', overwrite = 'all')
