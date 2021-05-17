31fcd2527dcc744196c4aa0ca5d0016c3d6b4582
allydunham <ally@ebi.ac.uk>
2021-05-17 16:22:22 +0100

    Refined plot

--- analysis/s_states.R
+++ analysis/s_states.R
@@ -9,18 +9,22 @@ foldx <- bind_rows(open = read_tsv("data/s_foldx/open/average.fxout"),
 classify <- function(open, closed) {
   out <- rep(NA, length(open))
   out[abs(closed) < 1 & abs(open) < 1] <- "Both Neutral"
-  out[abs(closed) > 1 & abs(open) > 1] <- "Both Significant"
-  out[abs(closed) < 1 & abs(open) > 1] <- "Closed Neutral & Open Significant"
-  out[abs(closed) > 1 & abs(open) < 1] <- "Closed Significant & Open Neutral"
+  out[abs(closed) >= 1 & abs(open) >= 1] <- "Both Significant"
+  out[abs(closed) < 1 & abs(open) >= 1] <- "Closed Neutral & Open Significant"
+  out[abs(closed) >= 1 & abs(open) < 1] <- "Closed Significant & Open Neutral"
   return(out)
 }
 
 comparison <- select(foldx, state, position, wt, mut, total_energy) %>%
   pivot_wider(names_from = state, values_from = total_energy) %>%
   mutate(diff = abs(open - closed),
-         type = classify(open, closed))
+         type = classify(open, closed)) %>%
+  drop_na()
 
 p <- ggplot(comparison, aes(x = open, y = closed)) +
-  geom_point(aes(colour = type), shape = 20) +
+  geom_point(aes(colour = type), shape = 20, size = 0.5) +
   geom_abline(slope = 1, intercept = 0) +
-  geom_smooth(method = "lm", formula = y ~ x)
+  geom_smooth(method = "lm", formula = y ~ x, size = 0.6) +
+  scale_color_brewer(name = "", palette = "Dark2") +
+  labs(x = "Open State", y = "Closed State")
+ggsave("figures/spike_states.pdf", p, units = "cm", width = 15, height = 9)
